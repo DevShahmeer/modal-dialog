acccd592eb681af3ae76f31796a1ab4f94edd1e0
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-21 23:33:41 +0500

    Set up Redux Toolkit and Modal components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import Modal from './Modal';
+import { Provider } from 'react-redux';
 import ModalContent from './ModalContent';
-import { ModalProvider } from './ModalContext';
+import {store} from './store';
 
 import './App.css';
 
 function App() {
   return (
-    <ModalProvider>
-      <div className="app">
-        <h1>Modal</h1>
-        <ModalContent />
-        <Modal>
-          <h2>Modal Content</h2>
-          <p>This is the content of the modal.</p>
-        </Modal>
+    <Provider store={store}>
+      <div className='app'>
+      <h1>Modal</h1>
+      <ModalContent/>
+      <Modal>
+        <h1>Modal Context</h1>
+        <p>This is the content of the App</p>
+      </Modal>
       </div>
-    </ModalProvider>
+    </Provider>
   );
 }
 
